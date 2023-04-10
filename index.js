699040d0c608d10c8151760c560b2463c91c6f48
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-04-10 16:28:42 +0200

    create enviroment variabels

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+const hostname = process.env.HOST_NAME;
 
-app.listen(5050, 'localhost', () => {
+app.listen(port, hostname, () => {
         console.log("Server is up & running on port ")
 });
 
