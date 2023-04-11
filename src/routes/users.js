8e74ed15c1ac1aa4b133a2184b4a8302d03266d9
Mohamed6061 <moemen1555@gmail.com>
2023-04-11 12:14:31 +0200

    modify

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,5 @@
 const { express } = require("../../index");
 const UsersRoute = express.Router();
-const { query_runner, query_error } = require("../helpers/mysql_helpers");
 
 UsersRoute.get("/", (req, res) => {
         
