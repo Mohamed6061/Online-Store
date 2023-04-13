92ad477feb9078b0bd5b0b0225b3069a20653f30
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-04-13 03:46:23 +0200

    create user

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,11 +1,17 @@
 const { express } = require("../../index");
 const UsersRoute = express.Router();
+const {create} =require("../helpers/sequelizeHelpers")
+const { User } = require('../../models/main');
 // const { query_runner, query_error } = require("../helpers/mysql_helpers");
 
-UsersRoute.get("/", (req, res) => {
-        
-        res.render("signup", {  page_title: "Sign Up" });
-
+UsersRoute.post("/", async (req, res) => {
+  try {
+  const data = await req.body
+  create(User ,data)
+  res.render("products", {products, page_title: "Products" });
+  } catch (error) {
+  console.error(error.message);
+  }
 });
 
 
