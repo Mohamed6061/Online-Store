5351cebe965e9dd0591f36ae42d4f3e1462db091
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-04-13 22:01:10 +0200

    create product_details request

--- src/routes/product_detail.js
+++ src/routes/product_detail.js
@@ -1,10 +1,21 @@
 const { express } = require("../../index");
 const UsersRoute = express.Router();
+const { getOne } = require("../helpers/sequelizeHelpers")
+const { Product } = require('../../models/main');
 
-UsersRoute.get("/", (req, res) => {
-        res.render("product_detail", {  page_title: "Product Details" });
 
+UsersRoute.get('/products/:id', async (req, res) => {
+  const productId = req.params.id;
+try{
+  getOne(Product ,productId)
+  res.render("product_detail", {  page_title: "Product Details" });
+}catch (error) {
+  console.error(error);
+  res.status(500).json({ message: 'Internal server error' });
+}
+  
+  
+  
 });
-
 
 module.exports = UsersRoute;
