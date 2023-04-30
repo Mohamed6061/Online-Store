b0906c5b16bc29efd2d253749ef892a3e5522cfc
Mohamed6061 <moemen1555@gmail.com>
2023-04-30 20:52:36 +0200

    Edite card

--- src/routes/card.js
+++ src/routes/card.js
@@ -1,114 +1,67 @@
 const { express } = require("../../index");
 const UsersRoute = express.Router();
+const { orders, orderitems,Product } = require('../../models/main');
 const app = express();
 
-
-UsersRoute.get("/" , (req, res) => {
-   res.render("card", { page_title: "Card"});
-});
-
-// const express = require('express');
-// const Sequelize = require('sequelize');
-
-// const app = express();
-// const port = 5050;
-
-// Configure the sequelize connection to the database
-// const sequelize = new Sequelize('database_name', 'username', 'password', {
-//     host: 'localhost',
-//     dialect: 'mysql' 
-// });
-
-// Define the OrderItem model using sequelize
-// const OrderItem = sequelize.define('orderItem', {
-//     name: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-// },
-//     price: {
-//     type: Sequelize.FLOAT,
-//     allowNull: false
-// }
-// });
-
 // Define the GET route of the card page
-app.get('/card', async (req, res) => {
-// Retrieve all order items from the database
-    // const orderItems = await OrderItem.findAll();
-
+UsersRoute.get('/', async (req, res) => {
+   // Retrieve all order items from the database
+   // get the id of the order with "not complete" status
+   const USERId = 1;
    
-   
-   const OrderItem = sequelize.define('OrderItem', {
-      name: {
-      type: Sequelize.STRING,
-      allowNull: false
-   },
-      price: {
-      type: Sequelize.FLOAT,
-      allowNull: false
-   }
-   });
-   
-   const Order = sequelize.define('Order', {
-      status: {
-      type: Sequelize.STRING,
-      allowNull: false
-   }
-   });
-   
-   Order.hasMany(OrderItem);
-   OrderItem.belongsTo(Order);
-   
-    // get the id of the order with "not complete" status
-   Order.findOne({
+   orders.findOne({
       where: {
-      status: 'not complete'
-   }
-   }).then(order => {
-   
-   const orderId = order.id;
-   
-      // Get products for the specified order
-      
-   OrderItem.findAll({
-      where: {
-      orderId: orderId
+         status: 'Not Complete',
+         userId: USERId
       }
-   }).then(orderItems => {
-      const products = orderItems.map(item => {
-      
-      return {
-      
-            price: item.price
-      };
-      });
-   
-      console.log(products);
+   }).then(order => {
+      if (order) {
+         const orderId = order.id;
+         // Get products for the specified order
+         return orderitems.findAll({
+            where: {
+               orderId: orderId
+            }
+         }).then( async orderItems => {
+            const CardProucts = orderItems.map(item => {
+               return {
+                  productId: item.productId,
+                  quantity: item.quantity,
+                  price: item.price
+               };
+            });
+
+            const products = await Product.findAll();
+            var Allproducts = products.map(product => ({
+               id: product.id,
+               name: product.name,
+               price: product.price,
+               description: product.description,
+               image: product.image
+            }));
+
+            res.render('card', { page_title: "Card", CardProucts , Allproducts });
+         });
+      } else {
+         console.log("Card is Empty")
+         res.status(401).send('<script>alert("Card is empty, Please go to product page to add product.");location.href="/products"</script>');
+      }
    });
-   });
-   
-// Display the card page with orderItems
-   res.render('card', { orderItems });
 });
 
 // Define the POST route for deleting the item from the order items table
-app.post('/delete-item/:id', async (req, res) => {
-// Extract the id from the parameters sent in the request
+UsersRoute.delete('/:id', async (req, res) => {
+   // Extract the id from the parameters sent in the request
    const id = req.params.id;
 
-// Search for the item to be deleted using the id
-   const orderItem = await OrderItem.findByPk(id);
+   // Search for the item to be deleted using the id
+   const orderItem = await orderitems.findOne({where: { productId: id }});
 
-// delete the item if it exists
+   // delete the item if it exists
    if (orderItem) {
       await orderItem.destroy();
-}
-
-// Redirect the user to the card page after deleting the item
-   res.redirect('/card');
+   }
+   res.status(204).send(`<script>alert("Deleted")</script>`);
 });
 
-// app.listen(port, () => {
-//     console.log(`Server is listening at http://localhost:${port}`);
-// });
 module.exports = UsersRoute;
