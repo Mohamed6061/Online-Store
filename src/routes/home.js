364eb002e19bbe20c275ad8f16e59ca192b89cd2
marwa aljmal <107772075+marwaaljmaal@users.noreply.github.com>
2023-05-03 11:06:24 +0300

    add home.js

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,9 +1,45 @@
 const { express } = require("../../index");
 const UsersRoute = express.Router();
 
-UsersRoute.get("/", (req, res) => {
-        res.render("home", {  page_title: "Home" });
+// UsersRoute.get("/", (req, res) => {
+//         res.render("home", {  page_title: "Home" });
+// });
+
+// const express = require('express');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database_name', 'username', 'password', {
+host: 'localhost',
+dialect: 'mysql'
+});
+
+const Product = sequelize.define('Product', {
+        name: {
+        type: Sequelize.STRING,
+        allowNull: false
+},
+        description: {
+        type: Sequelize.STRING,
+        allowNull: false
+},
+        price: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+}
 });
 
 
+
+UsersRoute.get("/", (req, res) => {
+        Product.findAll().then(products => {
+        const data = products.map(product => {
+                return {
+                name: product.name,
+                description: product.description,
+                price: product.price
+};
+});
+console.log(data)
+res.render("home", { page_title: "Home" });
+});
+});
 module.exports = UsersRoute;
